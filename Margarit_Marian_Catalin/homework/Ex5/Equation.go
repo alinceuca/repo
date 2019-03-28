4464f077632eed7cd5e0e8cf138f98d490d68541
AzureAD\CatalinMargarit <Catalin.Margarit@ecenta.com>
2019-03-28 17:46:08 +0200

    GO-Homework: Fix equation

--- Margarit_Marian_Catalin/homework/Ex5/Equation.go
+++ Margarit_Marian_Catalin/homework/Ex5/Equation.go
@@ -27,14 +27,12 @@ func main() {
 		b, _ := strconv.ParseFloat(r.FormValue("bCoef"),64)
 		c, _ := strconv.ParseFloat(r.FormValue("cCoef"),64)
 		solutions := make(chan float64, 2)
-		go s1(a, b, c, solutions)
-		go s2(a, b, c, solutions)
-		fmt.Fprintf(w, "X1= %f\nX2=%f", <-solutions,<-solutions)
+		fmt.Fprintf(w, "%s", solve(a, b, c, solutions))
 	})
 	http.ListenAndServe(":8080", nil)
 }
 
-func s1(a float64, b float64, c float64, solutions chan float64) {
+func solve(a float64, b float64, c float64, solutions chan float64) string{
 	var div float64
 	var quo float64
 
@@ -42,14 +40,23 @@ func s1(a float64, b float64, c float64, solutions chan float64) {
 	quo = div / (2 * a)
 
 	solutions <- quo
-}
+	delta := (math.Pow(b, 2))-(4*a*c)
 
-func s2(a float64, b float64, c float64, solutions chan float64) {
-	var div float64
-	var quo float64
-
-	div = (b * -1) - math.Sqrt((math.Pow(b, 2))-(4*a*c))
-	quo = div / (2 * a)
-
-	solutions <- quo
+	if delta > 0 {
+		x1 := ((-b) + math.Sqrt(delta)) / (2 * a)
+		x2 := ((-b) - math.Sqrt(delta)) / (2 * a)
+		return fmt.Sprintf("x1: %f  x2: %f", x1, x2)
+	}
+	if delta == 0 {
+		x := (-b) / (2 * a)
+		return fmt.Sprintf("x: %f", x)
+	}
+	if delta < 0 {
+		r := (-b) / (2 * a)
+		i := math.Sqrt(-delta) / (2 * a)
+		var x1 = complex(r, i)
+		var x2 = complex(r, -i)
+		return fmt.Sprintf("x1: %v  x2: %v", x1, x2)
+	}
+	return ""
 }
